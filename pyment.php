e617a2665b5093190549a3fc18449f5e11f780f3
lifaet <lifaethossain@gmail.com>
2022-08-09 01:23:08 +0600

    Update pyment.php

--- pyment.php
+++ pyment.php
@@ -53,4 +53,43 @@ $post_data['product_amount'] = "100";
 $post_data['vat'] = "5";
 $post_data['discount_amount'] = "5";
 $post_data['convenience_fee'] = "3";
+
+# REQUEST SEND TO SSLCOMMERZ
+$direct_api_url = "https://sandbox.sslcommerz.com/gwprocess/v4/api.php";
+
+$handle = curl_init();
+curl_setopt($handle, CURLOPT_URL, $direct_api_url );
+curl_setopt($handle, CURLOPT_TIMEOUT, 30);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 30);
+curl_setopt($handle, CURLOPT_POST, 1 );
+curl_setopt($handle, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE); # KEEP IT FALSE IF YOU RUN FROM LOCAL PC
+
+
+$content = curl_exec($handle );
+
+$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+if($code == 200 && !( curl_errno($handle))) {
+	curl_close( $handle);
+	$sslcommerzResponse = $content;
+} else {
+	curl_close( $handle);
+	echo "FAILED TO CONNECT WITH SSLCOMMERZ API";
+	exit;
+}
+
+# PARSE THE JSON RESPONSE
+$sslcz = json_decode($sslcommerzResponse, true );
+
+if(isset($sslcz['GatewayPageURL']) && $sslcz['GatewayPageURL']!="" ) {
+        # THERE ARE MANY WAYS TO REDIRECT - Javascript, Meta Tag or Php Header Redirect or Other
+        # echo "<script>window.location.href = '". $sslcz['GatewayPageURL'] ."';</script>";
+	echo "<meta http-equiv='refresh' content='0;url=".$sslcz['GatewayPageURL']."'>";
+	# header("Location: ". $sslcz['GatewayPageURL']);
+	exit;
+} else {
+	echo "JSON Data parsing error!";
+}
 ?>
