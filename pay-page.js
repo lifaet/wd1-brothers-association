9e0887e26827e52b8d64b5b31be354596d8b9cc8
lifaet <lifaethossain@gmail.com>
2022-09-27 15:45:45 +0600

    ff

--- pay-page.js
+++ pay-page.js
@@ -1,15 +1,23 @@
+var amountkey = window.location.search.split('=').pop()
+if(amountkey == 1000){
+  document.getElementById('amount_pass').selectedIndex=2
+}else if (amountkey == 800){
+  document.getElementById('amount_pass').selectedIndex=1
+}else{
+  document.getElementById('amount_pass').selectedIndex=0
+}
 
 function amountupdate() {
     if (typeof document !== "undefined") {
         var select = document.getElementById('amount_pass');
         var amount = select.options[select.selectedIndex].value;
     }
-
+    console.log(amount)
     return amount;
 }
 amountupdate();
-setInterval(() => {
-    let a = amountupdate();
-    console.log(a)
-}, 100);
+// setInterval(() => {
+//     let a = amountupdate();
+//     console.log(a)
+// }, 100);
 
